66d53a67f2ef69dfe60d3f463951f009fdfb7adb
Mark Ambro <wowmarkambro@gmail.com>
2020-12-02 00:06:02 -0600

    🐛 backend graphql server setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,16 @@
 // let's go!
+require('dotenv').config();
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO use express MW to handle cookies
+// TODO use express to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+});
