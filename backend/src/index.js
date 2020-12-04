ac1646e8f9fbfa4b7ee12e0e3670f9c78dac2ec8
Mark <wowmarkambro@gmail.com>
2020-12-04 15:10:39 -0600

    🚧 added items to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,14 @@ const server = createServer();
 // TODO use express MW to handle cookies
 // TODO use express to populate current user
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
-});
+);
